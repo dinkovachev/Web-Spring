4a5b8dbdee5ce84b35a0b2d36ea33b9f3ca6a981
Dinko Kovachev <dinko.kovachev.a56@learn.telerikacademy.com>
2024-01-03 15:06:58 +0200

    Init Commit BeerTagProject

---
+++ beerTagProjects/src/main/java/com/telerikacademy/web/beertagprojects/BeerTagProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.beertagprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerTagProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerTagProjectsApplication.class, args);
+	}
+
+}
---
+++ beerTagProjects/src/test/java/com/telerikacademy/web/beertagprojects/BeerTagProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.beertagprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerTagProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
